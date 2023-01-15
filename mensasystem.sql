b43252afc93c4f59ad8fe0f94ce8fba6866bfe27
fuhrti <fuhrerphilipp@gmx.de>
2023-01-15 12:56:58 +0100

    Systemeinträge entfernt, Import in Webhosting möglich

--- mensasystem.sql
+++ mensasystem.sql
@@ -1,28 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Aug 18, 2018 at 12:51 PM
--- Server version: 5.5.60-0+deb8u1
--- PHP Version: 7.0.31-1~dotdeb+8.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `mensasystem`
---
-CREATE DATABASE IF NOT EXISTS `mensasystem` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE `mensasystem`;
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_artikel`
@@ -304,7 +279,3 @@ ALTER TABLE `tbl_umsatz`
 --
 ALTER TABLE `tbl_user`
   ADD CONSTRAINT `userrolle` FOREIGN KEY (`rolle`) REFERENCES `tbl_userrolle` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
